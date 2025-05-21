5dad98bdee681cea131228bb21ef8d591dc6af90
Teemu Taivainen <teemu.taivainen@hotmail.fi>
2025-05-21 15:26:21 +0300

    Allow navigating back to previous frame

--- src/videomeg_browser/prototype_qtgraph.py
+++ src/videomeg_browser/prototype_qtgraph.py
@@ -8,17 +8,15 @@ except Exception as exc:
     else:
         raise
 
-from cv2.typing import NumPyArrayNumeric
-from numpy import dtype, floating, integer
-from qtpy.QtWidgets import QMainWindow, QApplication, QVBoxLayout, QWidget, QPushButton
-import pyqtgraph as pg
 import sys
-import cv2
-import numpy as np
 
+import cv2
+import pyqtgraph as pg
 from cv2.typing import MatLike
+from qtpy.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
 
 pg.setConfigOptions(imageAxisOrder="row-major")
+
 
 class VideoFile:
     """
@@ -38,13 +36,12 @@ class VideoFile:
         self.frame_width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         self.frame_height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-        self.frame_index = -1
-        # Read the first frame (increments frame_index)
-        self.current_frame = self.read_next_frame()
+        # Matches to cv2.CAP_PROP_POS_FRAMES and tells the index of the next frame to be read
+        self.next_frame_idx = 0
 
     def read_next_frame(self) -> MatLike | None:
         """
-        Read the next frame from the video file and set is as the current frame.
+        Read the next frame from the video file.
         """
         if not self.cap.isOpened():
             raise ValueError("Trying to read from a closed video file.")
@@ -53,33 +50,37 @@ class VideoFile:
         if not ret:
             # End of video?
             return None
-        
-        self.frame_index += 1
+
+        self.next_frame_idx += 1
         # Convert the frame from BGR to RGB format
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        self.current_frame = frame
         return frame
 
-    '''
-    def previous_frame(self):
+    def read_previous_frame(self) -> MatLike | None:
         """
-        Seek to the previous frame.
+        Read the frame before the last read frame from the video file.
         """
-        if self.frame_index > 0:
-            self.frame_index -= 1
-            self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.frame_index)
-        return self.next_frame()
+        if not self.cap.isOpened():
+            raise ValueError("Trying to read from a closed video file.")
 
-    def jump_to_frame(self, frame_number: int):
+        if self.next_frame_idx < 2:
+            print("Already at the first frame.")
+            return None
+
+        # Last read frame is one step back, the frame before that is two steps back
+        self._set_next_frame(self.next_frame_idx - 2)
+        return self.read_next_frame()
+
+    def _set_next_frame(self, frame_idx: int) -> None:
         """
-        Seek to an arbitrary frame number.
+        Set the next frame to be read from the video file.
         """
-        if 0 <= frame_number < self.frame_count:
-            self.frame_index = frame_number
-            self.cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
-        return self.next_frame()
-    '''
+        if frame_idx < 0 or frame_idx >= self.frame_count:
+            raise ValueError(f"Frame index out of bounds: {frame_idx}")
+
+        self.next_frame_idx = frame_idx
+        self.cap.set(cv2.CAP_PROP_POS_FRAMES, frame_idx)
 
 
 class VideoBrowser(QMainWindow):
@@ -97,15 +98,19 @@ class VideoBrowser(QMainWindow):
 
         # Create an ImageView widget to display the video frames
         self.im_view = pg.ImageView()
-        self.im_view.setImage(self.video.current_frame)
+        self.im_view.setImage(self.video.read_next_frame())
         layout.addWidget(self.im_view)
 
-        # Create a button
+        # Create a button for navigating to the next frame
         self.button = QPushButton("Next Frame")
         self.button.clicked.connect(self.display_next_frame)
         layout.addWidget(self.button)
 
-    
+        # Create a button for navigating to the previous frame
+        self.prev_button = QPushButton("Previous Frame")
+        self.prev_button.clicked.connect(self.display_previous_frame)
+        layout.addWidget(self.prev_button)
+
     def display_next_frame(self):
         """
         Display the next frame in the video.
@@ -114,17 +119,30 @@ class VideoBrowser(QMainWindow):
         if frame is None:
             print("End of video reached.")
             return
-        
+
         self.im_view.setImage(frame)
+
+    def display_previous_frame(self):
+        """
+        Display the previous frame in the video.
+        """
+        frame = self.video.read_previous_frame()
+        if frame is None:
+            print("Already at the first frame.")
+            return
+
+        self.im_view.setImage(frame)
+
 
 if __name__ == "__main__":
     app = QApplication([])
 
-    video = VideoFile("/u/69/taivait1/unix/video_meg_testing/Subject_2_Luna/export_video/animal_meg_subject_2_240614_with_audio.avi")
+    video = VideoFile(
+        "/u/69/taivait1/unix/video_meg_testing/Subject_2_Luna/export_video/animal_meg_subject_2_240614_cropped.avi"
+    )
+
     window = VideoBrowser(video)
     window.resize(800, 600)
     window.show()
 
     sys.exit(app.exec_())
-
-
