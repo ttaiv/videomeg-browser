9413f620f822e7e8d3fd17a918f7e962e5e95961
Teemu Taivainen <teemu.taivainen@hotmail.fi>
2025-05-26 15:30:48 +0300

    Calculate video slider position from meg slider position

--- src/videomeg_browser/mne_connector.py
+++ src/videomeg_browser/mne_connector.py
@@ -2,19 +2,76 @@ import os.path as op
 import sys
 
 import mne
+import numpy as np
 from mne_qt_browser._pg_figure import TimeScrollBar
 from qtpy import QtWidgets
 from qtpy.QtCore import Qt
 
-from .browser import VideoBrowser, VideoFile
+from .browser import VideoBrowser
+from .comp_tstamps import comp_tstamps
+from .video import VideoFile, VideoFileHelsinkiVideoMEG
+
+
+class TimeStampMapper:
+    """Maps time points from raw data to video frames."""
+
+    def __init__(
+        self, raw: mne.io.Raw, raw_timing_ch: str, video: VideoFileHelsinkiVideoMEG
+    ):
+        self.raw = raw
+        picks_timing = mne.pick_types(raw.info, meg=False, include=[raw_timing_ch])
+        dt_timing = raw[picks_timing, :][0].squeeze()
+
+        print("Beginning timestamp computation...")
+        self.raw_ts = comp_tstamps(dt_timing, raw.info["sfreq"])
+        print("Timestamp computation done.")
+        self.vid_ts = video.ts
+
+        print(f"Number of raw timestamps: {len(self.raw_ts)}")
+        print(f"Number of video timestamps: {len(self.vid_ts)}")
+
+    def raw_time_to_video_frame_index(self, raw_t: float) -> int | None:
+        """Convert a time point from raw data (in seconds) to video frame index."""
+        raw_idx = self.raw.time_as_index(raw_t, use_rounding=False)
+        if len(raw_idx) > 1:
+            raise ValueError(
+                f"Multiple indices found for raw timestamp {raw_t}: {raw_idx}. "
+                "This should not happen."
+            )
+        raw_idx = raw_idx[0]
+        print(f"Raw index for time {raw_t}: {raw_idx}")
+
+        # Convert raw index to unix timestamp in milliseconds
+        raw_ts = self.raw_ts[raw_idx]
+        print(f"Raw timestamp at index {raw_idx}: {raw_ts}")
+
+        # Now we have temporal location of the raw data point in same units as video
+        # timestamps, so we can compare them directly.
+
+        if raw_ts < self.vid_ts[0] or raw_ts > self.vid_ts[-1]:
+            print("Raw timestamp is out of video bounds, returning None.")
+            return None
+
+        # Find the first video frame index that is greater than
+        # or equal to the raw timestamp
+        # TODO: Consider what other methods could be used here
+        idx = np.searchsorted(self.vid_ts, raw_ts)
+
+        return int(idx)
 
 
 class ScrollBarSynchronizer:
     """Synchronize the scroll bar of the raw data browser with the video browser."""
 
-    def __init__(self, raw_scroll_bar: TimeScrollBar, vid_slider: QtWidgets.QSlider):
+    def __init__(
+        self,
+        raw_scroll_bar: TimeScrollBar,
+        vid_slider: QtWidgets.QSlider,
+        ts_mapper: TimeStampMapper,
+    ):
         self.raw_scroll_bar = raw_scroll_bar
         self.vid_slider = vid_slider
+        self.ts_mapper = ts_mapper
         self._syncing = False
 
         self.raw_scroll_bar.valueChanged.connect(self._sync_video_to_raw)
@@ -24,7 +81,20 @@ class ScrollBarSynchronizer:
         """Update the video position based on the raw data browser's scroll bar."""
         if not self._syncing:
             self._syncing = True
-            self.vid_slider.setValue(value)
+
+            print()
+            print(f"Syncing raw scroll bar value: {value}")
+            raw_t = value / self.raw_scroll_bar.step_factor
+            print(f"Corresponding raw time in seconds: {raw_t:.6f}")
+
+            vid_idx = self.ts_mapper.raw_time_to_video_frame_index(raw_t)
+            print(f"Corresponding video frame index: {vid_idx}")
+
+            if vid_idx is None:
+                print("No corresponding video frame found for this raw timestamp.")
+            else:
+                self.vid_slider.setValue(vid_idx)
+
             self._syncing = False
 
     def _sync_raw_to_video(self, value):
@@ -51,8 +121,12 @@ def plot_raw_with_video(raw: mne.io.Raw, video: VideoFile):
     dock.setFloating(True)
     raw_browser.addDockWidget(Qt.RightDockWidgetArea, dock)
 
+    ts_mapper = TimeStampMapper(raw, raw_timing_ch="STI016", video=video_file)
+
     # Sync the scroll bars
-    sync = ScrollBarSynchronizer(raw_browser.mne.ax_hscroll, video_browser.frame_slider)
+    sync = ScrollBarSynchronizer(
+        raw_browser.mne.ax_hscroll, video_browser.frame_slider, ts_mapper
+    )
 
     # Profit
     raw_browser.show()
@@ -62,10 +136,13 @@ def plot_raw_with_video(raw: mne.io.Raw, video: VideoFile):
 if __name__ == "__main__":
     base_path = "/u/69/taivait1/unix/video_meg_testing/Subject_2_Luna"
     # Create a video file object
-    video_file = VideoFile(op.join(base_path, "Video_MEG", "kapsu.mp4"))
+    video_file = VideoFileHelsinkiVideoMEG(
+        op.join(base_path, "Video_MEG", "animal_meg_subject_2_240614.video.dat")
+    )
 
     # Create a raw data object
     raw = mne.io.read_raw_fif(
         op.join(base_path, "Raw", "animal_meg_subject_2_240614.fif"), preload=True
     )
+
     plot_raw_with_video(raw, video_file)
